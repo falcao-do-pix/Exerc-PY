d6f6206844c17b050dd72ccd6833a92ac63396dd
Leonardo Silva <leonardofabricioavila@gmail.com>
2024-03-03 10:16:09 -0400

    Exercício 14 finalizado.

--- Exerc14.py
+++ Exerc14.py
@@ -16,5 +16,9 @@ os dados do programa com as mensagens adequadas.
 peso_peixes = float(input("Peso dos peixes (Kg): "))
 
 if (peso_peixes > 50):
-   excesso = peso_peixes * 50
-   print(excesso)
+   excesso = peso_peixes - 50
+   multa = excesso * 4
+   print(f"Quantidade de quilos além do limite (50Kg): {excesso}")
+   print(f"Multa: R${multa:.2f}")
+else:
+   print("Não há excesso de quilos.")

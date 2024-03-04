f0aa10c2c466672e16091c5e3fcfff6b6104f076
Leonardo Silva <118554112+LFDev01@users.noreply.github.com>
2024-03-04 08:20:11 -0400

    Update Exerc15.py

--- Exerc15.py
+++ Exerc15.py
@@ -1,9 +1,4 @@
 """
-Faculdade Martha Falcão Wyden
-Disciplina: Desenvolvimento Rápido de Aplicações em Python
-Aluno: Leonardo Fabrício de Ávila Silva
-Matrícula: 202203284649
-
 15) Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas
 no mês. Calcule e mostre o total do seu salário no referido mês, sabendo-se que são descontados
 11% para o Imposto de Renda, 8% para o INSS e 5% para o sindicato, faça um programa que nos
